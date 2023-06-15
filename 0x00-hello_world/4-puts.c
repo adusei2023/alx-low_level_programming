9147253d2c0939a807cfbd9a542edfd19183d5c7
adusei2023 <samuel.boateng01@yahoo.com>
2023-06-15 16:46:44 -0700

    update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - print a line of code using puts
+ * main - Print a line of code using puts
  *
- * Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
 	
