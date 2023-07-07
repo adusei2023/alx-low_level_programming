a6d5907de1d691725822a0ca3d9eb6f7059d0c1c
adusei2023 <samuel.boateng01@yahoo.com>
2023-07-07 15:14:32 -0700

    tasks completed

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,50 +1,30 @@
 #include "main.h"
-
-int get_length(char *s);
-
 /**
- * _strcat - Concatenates the string pointed to by @src,
- * including the terminating null byte,
- * to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- * Return: A pointer to the destination string @dest.
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int initial_end;
+	int i;
 	int j;
 
-	initial_end = get_length(dest);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[initial_end + j] = src[j];
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-	dest[initial_end + j] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
 
-/**
- * get_length - returns the length of a string
- * @s: String whose length we wish to determine
- *
- * Return: Length of s
- */
-int get_length(char *s)
-{
-	int length;
-	int i;
-
-	length = 0;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		++length;
-		i++;
-	}
-
-	return (length);
-}
